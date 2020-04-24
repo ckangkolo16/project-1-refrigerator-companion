26141256016409add3883645323bce2f1dbcb5ee
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-04-23 20:10:25 -0500

    updated the JS to allow us to search using the API testing Key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,16 @@
-$(document).ready(function () {
-  var recipeList = [];
-  $("#searchBtn").on("click", function () {
-    ///get value of what is in the input
-    let recipe = $("#searchRecipe").val();
-    //clear input box
-    $("#searchRecipe").val("");
-    //pass the variable into a function to run your ajax call and call that function
-    
+var recipeList = [];
+//Working filtered recipe search
+$("#searchBtn").click(function () {
+  console.log("You Clicked search");
+  var recipeSearched = $("#searchRecipe").val();
+  console.log(recipeSearched);
+  var queryURL =
+    "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + recipeSearched;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
   });
-
-function renderRecipes(recipe) {
-    var APIID = "d04073fe"
-    var APIKey = "fed5ca5263ce63fa1ad93a6a12d222f8";
-    
-    //Url to query database
-
-    var queryUrl = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY} "
-+ APIID; +
-      APIKey;
-
-    $.ajax({
-      url: queryUrl,
-      method: "GET",
-      dataType: "json",
-    }).then(function (response) {
-      console.log(response);
-      
-    });
-  }
-  $(".dropdown").on("click", function (event) {
-    event.stopPropagation();
-    $(this).toggleClass("is-active");
-  })
-})
+});
