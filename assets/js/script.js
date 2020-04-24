a2fc841097b8cc7261f0686cd344bc6b83c5868a
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-04-24 15:38:10 -0500

    upadating more js to allow for the recipe to be displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,85 @@
-function keyCode(event) {
-  const w = event.keyCode;
-  if (w == 13) {
-    console.log("you've hit enter");
-  } else {
-    return;
-  }
-}
-var recipeList = [];
-//Working filtered recipe search
-$("#searchBtn").click(function () {
-  var ingredientSearched = $("#searchRecipe").val();
-  console.log(ingredientSearched);
-  console.log("You are searching for " + ingredientSearched + ".");
-  var queryURL =
-    "https://www.themealdb.com/api/json/v1/1/filter.php?i=" +
-    ingredientSearched;
+$(document).ready(function () {
+  // function keyCode(event) {
+  //   const w = event.keyCode;
+  //   if (w == 13) {
+  //     console.log("you've hit enter");
+  //   } else {
+  //     return;
+  //   }
+  // }
+  $("#searchBtn").click(function () {
+    var ingredientSearched = $("#searchRecipe").val();
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    // console.log("respoonse: ", JSON.stringify(response));
-    //clear out current Carousel
-    $("#pictureCaro").empty();
-
-    var images = $("<img>").attr("src", mealPic).appendTo("body");
-    // $("movie-view").text(JSON.stringify(response);
-    //createElement div  with img for pictures
-    //for each in array place img into a
-    for (var i = 0; i < response.meals.length; ++i) {
-      var mealPic = response.meals[i].strMealThumb;
-      var mealName = response.meals[i].strMeal;
-      console.log(mealName);
-      console.log(mealPic);
-      $("#pictureCaro").append(
-        $("<div>")
-          .addClass("media-content")
-          .attr("id", "menu-card")
-          .attr("style", "max-width: 7rem; margin-left: 23px")
-
-          .append(
-            $("<img src=" + mealPic + ">")
-              .attr("height", "50px")
-              .attr("width", "50px")
-          )
-          .append($("<p>").html(mealName))
-      );
-
-      // $("#pictureCaro").attr(image);
-    }
-
-    // $('div').
+    // change v1 to v2 & add apiKey
+    searchByName(ingredientSearched);
   });
-});
-function renderChoices(meal) {
-  $("#finddddd").empty();
 
-  for (var i = 0; i <= 4; i++) {
-    console.log(i);
+  //populate the recipe area with recipe list
+  function searchByName(searchValue) {
+    var queryURL =
+      "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=" +
+      searchValue;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      dataType: "json",
+    }).then(function (response) {
+      //clear out current Carousel
+      $("#pictureCaro").empty();
 
-    //layout Variables for pic and title.
-    // var titleRecipe = moment().add(1 + i, 'days').format("MM/DD/YYY");
-    // var meal picture = response.meals[i].weather[0].icon;
-    // var mealPictureURL = "http://openweathermap.org/img/wn/" + cityIconForecastCode + "@2x.png";
-    // var cityTemperatureForecast = Math.round(response.list[i].main.temp);
-    // var cityHumidityForecast = response.list[i].main.humidity;
-    //display current meals information
-    function currentMealChoice(currentMealChosen) {}
-    //   forEach()
-    // response.meals
+      // var images = $("<img>").attr("src", mealPic).addClass("img-click");
+
+      // $("images").text(JSON.stringify(response);
+      //createElement div  with img for pictures
+      for (var i = 0; i < response.meals.length; ++i) {
+        var mealId = response.meals[i].idMeal;
+        console.log(mealId);
+        // images.attr("alt", mealName);
+        var mealPic = response.meals[i].strMealThumb;
+        var mealName = response.meals[i].strMeal;
+
+        $("#pictureCaro").append(
+          $("<div>")
+            .addClass("media-content")
+            .attr("id", "menu-card")
+            .attr("style", "max-width: 7rem; margin-left: 23px")
+
+            .append(
+              $("<img src=" + mealPic + ">")
+                .addClass("img-click")
+                .attr("height", "50px")
+                .attr("width", "50px")
+            )
+            .append($("<p>").html(mealName))
+        );
+
+        searchById(mealId);
+      }
+      $(".img-click").on("click", function () {
+        console.log("successful click");
+      });
+    });
   }
-}
-//create function that stores first few meals into array
-//display array
-//response = meals[0]
-// next day = moment().add(1 + i, 'days').format("MM/DD/YYY");
+
+  function searchById(id) {
+    $.ajax({
+      type: "GET",
+      url: `https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${id}`,
+      dataType: "json",
+      success: function (response) {
+        $(".img-click").on("click", function () {
+          $("#recipe-box").empty();
+          $("#recipe-box").removeClass("hide");
+
+          var recipeInt = $("<p>").text(response.meals[0].strInstructions);
+
+          $("#recipe-box").append(recipeInt);
+        });
+      },
+    });
+  }
+
+  function showRecipeDetails() {
+    //on img-click we display recipe box and append in recipe instructions
+  }
+});
